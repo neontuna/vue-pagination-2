9e67e9aa53b5258d1dd0d2eea9725c8855b07470
Justin Mullis <justin.mullis@gmail.com>
2018-03-08 11:18:27 -0500

    customize for semantic-ui

--- compiled/template.js
+++ compiled/template.js
@@ -13,56 +13,44 @@ module.exports = function () {
     if (this.chunksNavigation === 'fixed') {
 
       prevChunk = h(
-        'li',
-        { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + theme.prev + ' VuePagination__pagination-item-prev-chunk ' + this.allowedChunkClass(-1) },
-        [h(
-          'a',
-          { 'class': theme.link,
-            attrs: { href: 'javascript:void(0);',
-              disabled: !!this.allowedChunkClass(-1)
-            },
-            on: {
-              'click': this.setChunk.bind(this, -1)
-            }
+        'a',
+        { 'class': theme.link,
+          attrs: { href: 'javascript:void(0);',
+            disabled: !!this.allowedChunkClass(-1)
           },
-          ['<<']
-        )]
+          on: {
+            'click': this.setChunk.bind(this, -1)
+          }
+        },
+        ['<<']
       );
 
       nextChunk = h(
-        'li',
-        { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + theme.next + ' VuePagination__pagination-item-next-chunk ' + this.allowedChunkClass(1) },
-        [h(
-          'a',
-          { 'class': theme.link,
-            attrs: { href: 'javascript:void(0);',
-              disabled: !!this.allowedChunkClass(1)
-            },
-            on: {
-              'click': this.setChunk.bind(this, 1)
-            }
+        'a',
+        { 'class': theme.link,
+          attrs: { href: 'javascript:void(0);',
+            disabled: !!this.allowedChunkClass(1)
           },
-          ['>>']
-        )]
+          on: {
+            'click': this.setChunk.bind(this, 1)
+          }
+        },
+        ['>>']
       );
     }
 
     this.pages.map(function (page) {
       items.push(h(
-        'li',
-        { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(page) },
-        [h(
-          'a',
-          { 'class': theme.link + ' ' + this.activeClass(page),
-            attrs: { href: 'javascript:void(0)',
-              role: 'button'
-            },
-            on: {
-              'click': this.setPage.bind(this, page)
-            }
+        'a',
+        { 'class': theme.link + ' ' + this.activeClass(page),
+          attrs: { href: 'javascript:void(0)',
+            role: 'button'
           },
-          [page]
-        )]
+          on: {
+            'click': this.setPage.bind(this, page)
+          }
+        },
+        [page]
       ));
     }.bind(this));
 
@@ -72,57 +60,39 @@ module.exports = function () {
       [h(
         'nav',
         { 'class': '' + theme.nav },
-        [h(
-          'ul',
-          {
-            directives: [{
-              name: 'show',
-              value: this.totalPages > 1
-            }],
+        [prevChunk, h(
+          'a',
+          { 'class': theme.link,
+            attrs: { href: 'javascript:void(0);',
+              disabled: !!this.allowedPageClass(this.page - 1)
+            },
+            on: {
+              'click': this.prev.bind(this)
+            }
+          },
+          ['<']
+        ), items, h(
+          'a',
+          { 'class': theme.link,
+            attrs: { href: 'javascript:void(0);',
+              disabled: !!this.allowedPageClass(this.page + 1)
+            },
+            on: {
+              'click': this.next.bind(this)
+            }
+          },
+          ['>']
+        ), nextChunk]
+      ), h(
+        'p',
+        {
+          directives: [{
+            name: 'show',
+            value: parseInt(this.records)
+          }],
 
-            'class': theme.list + ' VuePagination__pagination' },
-          [prevChunk, h(
-            'li',
-            { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + theme.prev + ' VuePagination__pagination-item-prev-page ' + this.allowedPageClass(this.page - 1) },
-            [h(
-              'a',
-              { 'class': theme.link,
-                attrs: { href: 'javascript:void(0);',
-                  disabled: !!this.allowedPageClass(this.page - 1)
-                },
-                on: {
-                  'click': this.prev.bind(this)
-                }
-              },
-              ['<']
-            )]
-          ), items, h(
-            'li',
-            { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + theme.next + ' VuePagination__pagination-item-next-page ' + this.allowedPageClass(this.page + 1) },
-            [h(
-              'a',
-              { 'class': theme.link,
-                attrs: { href: 'javascript:void(0);',
-                  disabled: !!this.allowedPageClass(this.page + 1)
-                },
-                on: {
-                  'click': this.next.bind(this)
-                }
-              },
-              ['>']
-            )]
-          ), nextChunk]
-        ), h(
-          'p',
-          {
-            directives: [{
-              name: 'show',
-              value: parseInt(this.records)
-            }],
-
-            'class': 'VuePagination__count ' + theme.count },
-          [this.count]
-        )]
+          'class': 'VuePagination__count ' + theme.count },
+        [this.count]
       )]
     );
   };
--- src/template.js
+++ src/template.js
@@ -10,55 +10,51 @@ module.exports = function() {
 
     if (this.chunksNavigation==='fixed') {
      
-     prevChunk =  <li class={`VuePagination__pagination-item ${theme.item} ${theme.prev} VuePagination__pagination-item-prev-chunk ${this.allowedChunkClass(-1)}`}>
+     prevChunk =  
       <a class={theme.link} 
       href="javascript:void(0);"
       disabled={!!this.allowedChunkClass(-1)} 
       on-click={this.setChunk.bind(this,-1)}>&lt;&lt;</a>
-      </li>
      
-     nextChunk = <li class={`VuePagination__pagination-item ${theme.item} ${theme.next} VuePagination__pagination-item-next-chunk ${this.allowedChunkClass(1)}`}>
+     nextChunk = 
       <a class={theme.link} 
       href="javascript:void(0);"
       disabled={!!this.allowedChunkClass(1)}
       on-click={this.setChunk.bind(this,1)}>&gt;&gt;</a>
-      </li>;
-  
+      
     }
        
     this.pages.map(function(page) {
       items.push(
-        <li class={`VuePagination__pagination-item ${theme.item} ${this.activeClass(page)}`}>
         <a class={`${theme.link} ${this.activeClass(page)}`} 
         href="javascript:void(0)"
         role="button"
         on-click={this.setPage.bind(this, page)}>{page}</a>
-        </li>
       )
     }.bind(this));
     
     return <div class={`VuePagination ${theme.wrapper}`}><nav class={`${theme.nav}`}>
-    <ul v-show={this.totalPages>1}
-    class={`${theme.list} VuePagination__pagination`}>
+
     {prevChunk} 
-    <li class={`VuePagination__pagination-item ${theme.item} ${theme.prev} VuePagination__pagination-item-prev-page ${this.allowedPageClass(this.page-1)}`}>
+
     <a class={theme.link} 
     href="javascript:void(0);"
     disabled={!!this.allowedPageClass(this.page-1)} 
     on-click={this.prev.bind(this)}>&lt;</a>
-    </li>
+
     {items}
-    <li class={`VuePagination__pagination-item ${theme.item} ${theme.next} VuePagination__pagination-item-next-page ${this.allowedPageClass(this.page+1)}`}>
+
     <a class={theme.link} 
     href="javascript:void(0);"
     disabled={!!this.allowedPageClass(this.page+1)} 
     on-click={this.next.bind(this)}>&gt;</a>
-    </li>
+
     {nextChunk}
-    </ul>
+
+    </nav>
+
     <p v-show={parseInt(this.records)}
     class={`VuePagination__count ${theme.count}`}>{this.count}</p>
-    </nav>
     </div>
   }
 }
